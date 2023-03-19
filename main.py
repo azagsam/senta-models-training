57c67137fb9cf2d480d545dbadd1f7454674ba21
Ales Zagar <ales.zagar@hotmail.com>
2023-03-19 08:34:31 +0100

    finished main script

--- main.py
+++ main.py
@@ -20,34 +20,34 @@ def main():
     sent_pretrained_model = SentenceTransformer('sentence-transformers/LaBSE')
     d2v_pretrained_model = Doc2Vec.load('model/doc2vec/model')
 
-    with open('results-sentence.txt', mode='w', encoding='utf-8') as f:
-        eno = eno.sample(frac=1).reset_index(drop=True)
-        for idx, row in eno.iterrows():
-            # filter data
-            end_date = row['date']
-            start_date = end_date - pd.DateOffset(days=5)
-            window = pd.date_range(start=start_date, end=end_date)
-            candidates = org[org['date'].isin(window)].reset_index()
+    eno = eno.sample(frac=1, random_state=42).reset_index(drop=True)
+    days = 7
+    print(f'Time window: {days} days ')
+    for idx, row in eno.iterrows():
+        # filter data
+        end_date = row['date']
+        start_date = end_date - pd.DateOffset(days=days)
+        window = pd.date_range(start=start_date, end=end_date)
+        candidates = org[org['date'].isin(window)].reset_index()
 
-            headline_results = headline_model(sent_pretrained_model, row, candidates)
-            body_unranked_results = body_model(sent_pretrained_model, row, candidates, ranking=False, n_sent=0)
-            body_ranked_results = body_model(sent_pretrained_model, row, candidates, ranking=True, n_sent=10)
-            d2v_results = d2v_model(d2v_pretrained_model, row, candidates)
+        # calculate
+        headline_results = headline_model(sent_pretrained_model, row, candidates.copy(deep=True))
+        body_unranked_results = body_model(sent_pretrained_model, row, candidates.copy(deep=True), ranking=False, n_sent=0)
+        body_ranked_results = body_model(sent_pretrained_model, row, candidates.copy(deep=True), ranking=True, n_sent=10)
+        d2v_results = d2v_model(d2v_pretrained_model, row, candidates.copy(deep=True))
 
-            print()
-
-            results = {'headline': headline_results,
-                       'body_unranked': body_unranked_results,
-                       'body_ranked': body_ranked_results,
-                       'doc2vec': d2v_results
-                       }
-            for exp, result in results.items():
-                print('------------------------')
-                print(exp.upper(), '\n\n')
-                print('ENOSTAVNO:', row['title'], '\n\n')
-                for title in result['title'][:5]:
-                    print(title, '\n\n')
-
+        # results
+        results = {'headline': headline_results,
+                   'body_unranked': body_unranked_results,
+                   'body_ranked': body_ranked_results,
+                   'doc2vec': d2v_results
+                   }
+        for exp, result in results.items():
+            print('------------------------')
+            print(exp.upper(), '\n\n')
+            print('ENOSTAVNO:', row['date'], row['title'], '\n\n')
+            for can_idx, can_row in result[:5].iterrows():
+                print(can_row['date'], can_row['title'], can_row['similarity'], '\n')
 
 
 if __name__ == '__main__':
