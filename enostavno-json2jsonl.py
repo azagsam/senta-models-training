33a02626b699412cea143d64e1314c971505c054
Ales Zagar <ales.zagar@hotmail.com>
2023-03-27 15:15:03 +0200

    script parses url

--- enostavno-json2jsonl.py
+++ enostavno-json2jsonl.py
@@ -2,6 +2,7 @@ import json
 import os
 
 import pandas as pd
+import re
 
 map_months = {
     'januar': 1,
@@ -29,9 +30,27 @@ def get_date(metadata):
         return None
 
 
+def get_url(file):
+    html_path = file.split('/')[-1].replace('.json', '.html')
+    full_path = os.path.join('data/crawl-enostavno-27-02-2023/cache', html_path)
+    with open(full_path, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith('<link rel="canonical" href='):
+                href_regex = r'href="([^"]+)"'
+                match = re.search(href_regex, line)
+                if match is not None:
+                    href = match.group(1)
+                    return href
+
+
 docs = []
 for file in os.scandir('data/crawl-enostavno-27-02-2023/cache_processed'):
     doc = json.load(open(file.path))
+
+    url = get_url(file.path)
+    doc['url'] = url
+
     date = get_date(doc['metadata'])
     body = " ".join(doc['article'])
     doc['date'] = date
